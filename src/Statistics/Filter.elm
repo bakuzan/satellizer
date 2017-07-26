f0ce21a2e729f7a4f3a8f7f12dbca48f62887a20
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-26 13:20:36 +0100

    Added filter links
    Need to sort out the correct url settings

--- src/Statistics/Filter.elm
+++ src/Statistics/Filter.elm
@@ -1,7 +1,7 @@
 module Statistics.Filter exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, classList, href)
 
 import Msgs exposing (Msg)
 
@@ -10,5 +10,12 @@ view : Html Msg
 view =
     div [ class "list-filter" ]
         [ text "LIST FILTERS WILL GO HERE"
+        , div [ class "button-group" ]
+              [ viewFilterLink "anime" 
+              , viewFilterLink "manga"
+              ]
         ]
 
+viewFilterLink : Html Msg
+viewFilterLink str =
+    a [ class "button-link", classList [("active", False)], href ("" ++ str) ] [ text str ]
