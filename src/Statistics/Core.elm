ffac789edef375a885b9b57c25a3be818768b786
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-26 13:27:05 +0100

    Restructuring view
    The value access will change when I sort the api call data.

--- src/Statistics/Core.elm
+++ src/Statistics/Core.elm
@@ -13,8 +13,10 @@ view : WebData CountData -> Html Msg
 view status =
     div []
         [ Statistics.Filter.view
-		, viewStatusBreakdown status.data
-        , viewHistoryTable status.data
+        , div [ class "flex-column flex-grow" ]
+              [ viewStatusBreakdown status.data
+              , viewHistoryTable status.data
+              ]
         ]
 
 viewStatusBreakdown : Counts -> Html Msg
@@ -23,14 +25,25 @@ viewStatusBreakdown data =
       counts =
         [1,2,3,4,5]
 
-      progressSegment num =
-        div [ class "percentage-breakdown__bar" ]
-            [ div [ style [("width", (toString num) ++ "%")] ] []
-            ]
-
     in
-      div [ class "percentage-breakdown" ]
-          (List.map progressSegment counts)
+      viewProgressBar counts
+
+
+-- Progress bar
+
+viewProgressBar : List Int -> Html Msg
+viewProgressBar values =
+  div [ class "percentage-breakdown" ]
+      (List.map viewProgressSegment values)  
+
+
+viewProgressSegment : Int -> Html Msg
+viewProgressSegment num =
+  div [ class "percentage-breakdown__bar", style [("width", (toString num) ++ "%")] ]
+      []
+
+
+-- History breakdown
 
 viewHistoryTable : WebData CountData -> Html Msg
 viewHistoryTable data =
