82fb2bbf4408d023d1ab83fc81dfa9538d1be05a
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-27 12:02:55 +0100

    Added month and season lists

--- src/Utils/Constants.elm
+++ src/Utils/Constants.elm
@@ -12,3 +12,34 @@ type =
   { anime = "anime
   , manga = "manga"
   }
+
+type alias Header = 
+  { name: String
+  , number: Int
+  }
+
+months : List Header
+months = 
+  [{ name = "Jan", number = 0 }
+  ,{ name = "Feb", number = 1 }
+  ,{ name = "Mar", number = 2 }
+  ,{ name = "Apr", number = 3 }
+  ,{ name = "May", number = 4 }
+  ,{ name = "Jun", number = 5 }
+  ,{ name = "Jul", number = 6 }
+  ,{ name = "Aug", number = 7 }
+  ,{ name = "Sep", number = 8 }
+  ,{ name = "Oct", number = 9 }
+  ,{ name = "Nov", number = 10 }
+  ,{ name = "Dec", number = 11 }
+  ]
+  
+seasons : List Header
+seasons = 
+  [{ name = "Winter", number = 3 }
+  ,{ name = "Spring", number = 6 }
+  ,{ name = "Summer", number = 9 }
+  ,{ name = "Fall" number = 12 }
+  ]
+  
+  
