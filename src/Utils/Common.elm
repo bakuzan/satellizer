09c967b4ff72f3ff8ab375fa98512e56da34e339
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-28 12:11:26 +0100

    Added split list

--- src/Utils/Common.elm
+++ src/Utils/Common.elm
@@ -1,6 +1,14 @@
 module Utils.Common exposing (..)
 
+
 replace : String -> String -> String -> String
 replace from to str =
     String.split from str
         |> String.join to
+
+
+splitList : Int -> List a -> List (List a)
+splitList i list =
+  case List.take i list of
+    [] -> []
+    listHead -> listHead :: splitList i (List.drop i list)
