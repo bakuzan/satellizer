835a61e8df9c2b99295daa2da436b1dd32c88ded
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-31 11:12:46 +0100

    Added ratings progress bars

--- src/Statistics/Core.elm
+++ src/Statistics/Core.elm
@@ -8,6 +8,7 @@ import Statistics.Filter
 import Statistics.HistoryTable
 import Models exposing (Model, CountData)
 import RemoteData exposing (WebData)
+import Utils.Common as Common
 
 
 viewRender : WebData CountData -> CountData
@@ -39,14 +40,43 @@ view model =
         ]
 
 
+calculateTotalOfValues : CountData -> Int
+calculateTotalOfValues list = 
+  List.map (\x -> x.value) list
+    |> List.foldr (+) 0
+
+
 viewStatus : CountData -> Html Msg
 viewStatus list =
   let
     total =
-     List.map (\x -> x.value) list
-      |> List.foldr (+) 0
+      calculateTotalOfValues list
 
   in
     div [id "status-container"]
         [General.ProgressBar.viewProgressBar total list
         ]
+
+
+viewRatings : CountData -> Html Msg
+viewRatings list =
+  let
+    total =
+      calculateTotalOfValues list
+    
+    ratings = 
+      Common.split 1 list
+    
+    viewRatingBar = 
+      viewSingleRating total
+    
+  in
+    div [id "status-container"]
+        [ List.map viewRatingBar ratings
+        ]
+
+
+viewSingleRating : Int -> CountData -> Html Msg
+viewSingleRating total rating = 
+  General.ProgressBar.viewProgressBar total rating
+  
