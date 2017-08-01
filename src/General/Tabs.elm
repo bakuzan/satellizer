c96c87e63f27ab041a26fda3dfbd68300093d7a3
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-01 10:14:11 +0100

    Update Tabs.elm

--- src/General/Tabs.elm
+++ src/General/Tabs.elm
@@ -1,7 +1,7 @@
-module General.Tabs exposing (..)
+module General.Tabs exposing (viewTabContainer)
 
 import Html exposing (..)
-import Html.Attributes exposing (type_, class, role)
+import Html.Attributes exposing (type_, class)
 
 
 
@@ -16,11 +16,11 @@ viewTabControls : List (String, List (Html Msg)) -> Html Msg
 viewTabControls tabList = 
   let
     generateTabButton tab = 
-      li [classList [("active", (first tab) == "history")], role "tab"] 
+      li [classList [("active", (first tab) == "history")]] 
          [ button [type_ "button", class "button"] 
                   [text (first tab)]
          ]
-    
+    --, role "tab"
   in
   ul [class "tab-controls row", role "tablist"]
      ([] ++ List.map generateTabButton tabList)
@@ -30,9 +30,9 @@ viewTabBodys : List (String, List (Html Msg)) -> Html Msg
 viewTabBodys tabList = 
   let
     generateTabBody tab = 
-      div [class "tab", role "tabpanel"]
+      div [class "tab"]
           [(second tab)]
-          
+    --, role "tabpanel"
   in
   div [class "tabs"] 
       ([] ++ List.map generateTabBody tabList)
