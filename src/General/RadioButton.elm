a9c354e1b42928108d7ac0abe93a025423c359b5
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-01 12:58:38 +0100

    Added user interaction to radio buttons

--- src/General/RadioButton.elm
+++ src/General/RadioButton.elm
@@ -2,6 +2,7 @@ module General.RadioButton exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 import Msgs exposing (Msg)
 import Utils.Common as Common
 
@@ -12,20 +13,25 @@ type alias RadioOption =
   }
 
 
-viewRadioGroup : String -> String -> List RadioOption -> Html Msg
-viewRadioGroup groupName groupValue options =
+viewRadioGroup : String -> String -> Msg -> List RadioOption -> Html Msg
+viewRadioGroup groupName groupValue message options =
   let
     radioOption =
-      viewRadioOption groupName groupValue
+      viewRadioOption groupName groupValue message
 
   in
   div [class "radio-group", Common.setRole "radiogroup"]
       ([] ++ List.map radioOption options)
 
 
-viewRadioOption : String -> String -> RadioOption -> Html Msg
-viewRadioOption groupName groupValue option =
+viewRadioOption : String -> String -> Msg -> RadioOption -> Html Msg
+viewRadioOption groupName groupValue message option =
+  let
+    optionValue = 
+      option.optionValue
+      
+  in
   label [class "radio", Common.setRole "radio"]
-        [ input [type_ "radio", name groupName, value option.optionValue, checked (option.optionValue == groupValue) ] []
+        [ input [type_ "radio", name groupName, value optionValue, checked (optionValue == groupValue), onClick (message optionValue) ] []
         , span [] [text option.label]
         ]
