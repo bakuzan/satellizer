22228232cc368dc4e173ff9189b4c27406213f87
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-16 15:31:35 +0100

    Add progress percentages for single segments
    Untested.

--- src/General/ProgressBar.elm
+++ src/General/ProgressBar.elm
@@ -5,12 +5,23 @@ import Html.Attributes exposing (class, style, title, attribute)
 import Msgs exposing(Msg)
 import Models exposing(CountData, Count)
 import Utils.Common exposing (divide)
+import Round
 
 
 viewProgressBar : Int -> CountData -> Html Msg
 viewProgressBar total values =
+let
+  segments = 
+    List.map (viewProgressSegment total) values
+    
+  progressItems = 
+    if (List.length values) > 1
+      then segments
+      else (segments ++ [ span [] [text (singleSegmentPercentage values total)]
+                        ])
+in
   div [ class "percentage-breakdown" ]
-      (List.map (viewProgressSegment total) values)
+      ([] ++ progressItems)
 
 
 viewProgressSegment : Int -> Count -> Html Msg
@@ -24,3 +35,17 @@ viewProgressSegment total pair =
 getPercentage : Int -> Int -> String
 getPercentage value total =
   (toString ((divide value total) * 100)) ++ "%"
+
+
+singleSegmentPercentage : CountData -> Int -> String
+singleSegmentPercentage values total = 
+  let
+    returnPercentage num = 
+      Round.round 2 (toFloat (getPercentage num total))
+      
+  in
+  List.head values
+    |> Maybe.withDefault { key = "", value = 0 }
+    |> .value
+    |> returnPercentage
+  
