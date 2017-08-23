673c14e8b13217e3f3d5e05bd57d4163841c98a6
bakuzan <imannoyed4days@hotmail.co.uk>
2017-08-23 10:15:48 +0100

    Removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import './styles/accordion.css';
 import './styles/main.css';
 import { Main } from './Main.elm';
 
-console.log("SATELLIZER >> ", Main);
+
 export default Main;
-// Main.embed(document.getElementById('satellizer'));
