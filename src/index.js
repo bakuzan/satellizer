7e82fbea61ba714445e34f790693b3479b8a7ca0
bakuzan <imannoyed4days@hotmail.co.uk>
2018-02-08 12:23:03 +0000

    reset project to non-ejected.

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import { Main } from './Main.elm';
 
 
 export default Main;
+
+if (process.env.NODE_ENV === "development") {
+  console.log("%c Your Elm App is running in development mode!", "color: maroon; font-size: 16px; font-weight: bold;")
+  Main.embed(document.getElementById("satellizer"), {
+    contentType: "anime",
+    isAdult: false,
+  })
+}
